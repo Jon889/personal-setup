67c1ecb4307723619b727d340cb19bdba568f984
Jonathan Bailey <Jon889@users.noreply.github.com>
2018-09-10 11:00:29 +0100

    Update clickmenu.sh

--- clickmenu.sh
+++ clickmenu.sh
@@ -5,15 +5,32 @@ function ctrl_c() {
 	echo -e "\033[?1003l"
 }
 
+# Make sure there is room for menu
+counter=$#
+while (( counter > 0 ))
+do
+	echo
+	(( counter-- ))
+done
+
+# Get the current row (after potentially scrolling to make room)
+
 IFS=';' read -sdR -p $'\033[6n' ROW COL
 ROW=${ROW#*[}
+# Subtract the number of items to get the row of the first item
+ROW=$(( $ROW - $# ))
+
+# Go to the top of the empty space.
+echo -ne "\033[$ROW;0H"
 
 DEL_LINE="\033[0K\r"
 DEF_BKGD="\033[49m"
 DGY_BKGD="\033[100m"
 LGY_BKGD="\033[47m"
 
-titles=$(echo "$@" | sed 's/ /\\n/g')
+titles=$(echo "$@" | awk '{for (i=1;i<=NF;i++) printf("%d %s\n", i-1, $i)}' )
+
+# Print the menu for the first time and turn on mouse reporting
 
 echo -e "$titles\033[?1003h"
 read -s -n 6 POS
@@ -31,13 +48,14 @@ do
 			LEN=${#title}
 			if (( $Y == (( ROW + IDX )) && $X <= $LEN)); then
 				if [[ "${POS:3:1}" = " " ]]; then
-					echo -ne "$DEL_LINE$DGY_BKGD$title$DEF_BKGD"
-					selected=$(echo "$title" | sed -E "s/[[:cntrl:]]\[m//g")
+					echo -ne "$DEL_LINE$DGY_BKGD$IDX $title$DEF_BKGD"
+					# Remove color control sequences
+					selected=$(echo "$title" | sed -E "s/[[:cntrl:]]\[[0-9]*m//g")
 				else
-					echo -ne "$DEL_LINE$LGY_BKGD$title$DEF_BKGD"
+					echo -ne "$DEL_LINE$LGY_BKGD$IDX $title$DEF_BKGD"
 				fi
 			else
-				echo -ne "$DEL_LINE$title"
+				echo -ne "$DEL_LINE$IDX $title"
 			fi
 			echo -ne "\n\033[2K\r"
 			(( IDX++ ))

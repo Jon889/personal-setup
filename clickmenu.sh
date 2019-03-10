a7cb16dd85d6bf015e029a08cda0734f7c120ec0
Jonathan Bailey <Jon889@users.noreply.github.com>
2019-03-10 19:01:54 +0000

    Fix clickmenu.sh

--- clickmenu.sh
+++ clickmenu.sh
@@ -2,7 +2,7 @@
 trap ctrl_c EXIT
 
 function ctrl_c() {
-	echo -e "\033[?1003l"
+	echo -e "\033[?1003l$DEF_BKGD"
 }
 
 # Make sure there is room for menu
@@ -31,53 +31,73 @@ LGY_BKGD="\033[47m"
 titles=$(echo "$@" | awk '{for (i=1;i<=NF;i++) printf("%d %s\n", i, $i)}' )
 
 # Print the menu for the first time and turn on mouse reporting
+DELIMETER=$(echo -ne '\033')
 
 echo -e "$titles\033[?1003h"
 read -s -n 6 POS
+
+
+LASTHIGHLIGHT=-1
 while true
 do
 	selected=""
-	if (( ${#POS} == 6 )); then
+	if [[ $POS == *"$DELIMETER"* ]]; then
 
-		X=$(($(printf '%d' "'${POS:4:1}") - 32));
-		Y=$(($(printf '%d' "'${POS:5:1}") - 32));
+		#This is needed because sometimes the code isn't the correct length and is invalid for some reason.
+		IFS=$DELIMETER read -s -a TPOS -n 6 <<< "$POS"
+		BADCOUNT=${#TPOS[1]}
+		while (( BADCOUNT != 5 ))
+		do
+			read -s -n $((5 - ${#TPOS[2]})) REMAININGCHARS
+			POS="$DELIMETER${TPOS[2]}$REMAININGCHARS"
 
-		echo -ne "\033[$ROW;0H"
-		IDX=0
-		for title in "$@"; do
-			LEN=${#title}
-			if (( $Y == (( ROW + IDX )) && $X <= $LEN)); then
-				if [[ "${POS:3:1}" = " " ]]; then
-					echo -ne "$DEL_LINE$DGY_BKGD$IDX $title$DEF_BKGD"
-					# Remove color control sequences
-					selected=$(echo "$title" | sed -E "s/[[:cntrl:]]\[[0-9]*m//g")
-				else
-					echo -ne "$DEL_LINE$LGY_BKGD$((IDX + 1)) $title$DEF_BKGD"
-				fi
-			else
-				echo -ne "$DEL_LINE$((IDX + 1)) $title"
-			fi
-			echo -ne "\n\033[2K\r"
-			(( IDX++ ))
+			IFS=$DELIMETER read -s -a TPOS -n 6 <<< "$POS"
+			BADCOUNT=${#TPOS[1]}
 		done
+
+		if (( ${#POS} == 6 )); then
+
+			X=$(($(printf '%d' "'${POS:4:1}") - 32));
+			Y=$(($(printf '%d' "'${POS:5:1}") - 32));
+
+			IDX=$((Y - ROW + 1))
+
+			if (( LASTHIGHLIGHT != Y && LASTHIGHLIGHT != -1 )); then
+				LHIDX=$(( LASTHIGHLIGHT - ROW + 1 ))
+				echo -ne "\033[$LASTHIGHLIGHT;0H$DEL_LINE$LHIDX ${!LHIDX}$DEF_BKG"
+			fi
+
+			if (( IDX >= 1 && IDX <= ${#@} )); then
+
+				echo -ne "\033[$Y;0H$DEL_LINE$LGY_BKGD$IDX ${!IDX}$DEF_BKG"
+
+				LASTHIGHLIGHT=$Y
+
+				if [[ "${POS:3:1}" = " " ]]; then
+					selected=$(echo "${!IDX}" | sed -E "s/[[:cntrl:]]\[[0-9]*m//g")
+					#read mouse up
+					read -s -n 6 POS
+				fi
+			fi
+
+			echo -ne "\033[$(( ROW + ${#@} )));0H$DEL_LINE"
+		fi
 	else
 		case $POS in
 		[0-9]|[0-9][0-9])
-			branch=$(echo "${!POS}" | sed -E "s/[[:cntrl:]]\[[0-9]*m//g")
-			git checkout "$branch"
+			selected=$(echo "${!POS}" | sed -E "s/[[:cntrl:]]\[[0-9]*m//g")
 			;;
 		d|dev)
-			git checkout develop
+			selected="develop"
 			;;
 		-)
-			git checkout "$OLD_GBNAME"
+			selected=$OLD_GBNAME
 			;;
 		esac
-		exit
-		break
 	fi
 	if [[ ! -z "$selected" ]]; then
 		echo -e "\033[?1003l"
+		echo "Selected: $selected"
 		git checkout "$selected"
 		exit
 		break
